920e5a6d2b005af011ff9611b63388f9209567df
SANTIAGO YOSA GONZALEZ <santiagoyosa84488@correo.itm.edu.co>
2019-09-03 22:06:38 -0500

    Create Owners

---
+++ MyVet.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVet.Web.Data.Entities;
+
+namespace MyVet.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
