8c4ef6cf5bbcf05066bc74593b25ffc42bbe0a4a
SANTIAGO YOSA GONZALEZ <santiagoyosa84488@correo.itm.edu.co>
2019-09-05 00:19:56 -0500

    Logout y authorize

--- MyVet.Web/Data/SeedDb.cs
+++ MyVet.Web/Data/SeedDb.cs
@@ -23,8 +23,8 @@ namespace MyVet.Web.Data
         {
             await _dataContext.Database.EnsureCreatedAsync();
             await CheckRoles();
-            var manager = await CheckUserAsync("1010", "Juan", "Zuluaga", "jzuluaga55@gmail.com", "350 634 2747", "Calle Luna Calle Sol", "Admin");
-            var customer = await CheckUserAsync("2020", "Juan", "Zuluaga", "jzuluaga55@hotmail.com", "350 634 2747", "Calle Luna Calle Sol", "Customer");
+            var manager = await CheckUserAsync("1010", "Santiago", "Yosa", "santiyosa@gmail.com", "350 634 2747", "Calle Luna Calle Sol", "Admin");
+            var customer = await CheckUserAsync("2020", "Gloria", "Guevara", "ardyosa@hotmail.com", "350 634 2747", "Calle Luna Calle Sol", "Customer");
             await CheckPetTypesAsync();
             await CheckServiceTypesAsync();
             await CheckOwnerAsync(customer);
